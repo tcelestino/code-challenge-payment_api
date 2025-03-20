83c457f1986d7152fc217706575dd49850ca7fab
Tiago Celestino <tcelestino@gmail.com>
2025-03-20 06:23:30 -0300

    refactor: remove unused function

--- mocks/fixtures/payment.js
+++ mocks/fixtures/payment.js
@@ -1,17 +1,8 @@
 const { v4: uuidv4 } = require('uuid');
 
-let currentTxId = uuidv4();
-
-const randomId = () => {
-  setInterval(() => {
-    currentTxId = uuidv4();
-  }, 1000);
-  return currentTxId;
-};
-
 module.exports = {
   payment: {
-    tx_id: randomId(),
+    tx_id: uuidv4(),
     status: 'pending',
   },
 };

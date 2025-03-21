cd26c47462611bfc7949fa4b04d135caae413d26
Tiago Celestino <tcelestino@gmail.com>
2025-03-21 17:18:12 -0300

    chore: setup to unit test

--- tests/setup.ts
+++ tests/setup.ts
@@ -5,16 +5,34 @@ vi.mock('@infrastructure/cache/redis', () => ({
   cacheGet: vi.fn().mockResolvedValue(null),
 }));
 
-// vi.mock('@config/environment', () => ({
-//   config: {
-//     externalService: {
-//       url: 'http://localhost:9000',
-//     },
-//     cache: {
-//       key: 'test-payment',
-//     },
-//   },
-// }));
+//TODO: setup database using testcontainers
+// this fix the issue of not able to connect to database in integration test
+
+vi.mock('@config/environment', () => ({
+  config: {
+    externalService: {
+      url: 'http://localhost:9000',
+    },
+    cache: {
+      key: 'test-payment',
+    },
+    database: {
+      host: 'localhost',
+      port: 5432,
+      name: 'test',
+      user: 'test',
+      password: 'test',
+    },
+    server: {
+      port: 3000,
+      nodeEnv: 'test',
+    },
+    redis: {
+      host: 'localhost',
+      port: 6379,
+    },
+  },
+}));
 
 global.fetch = vi.fn();
 
